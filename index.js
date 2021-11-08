fc194c6ff5bb05401195950351a34fe021f20e5c
DEVS-NGHIANGO <nghia.ngo@cloudteam.vn>
2021-11-08 10:28:27 +0700

    UI

--- index.js
+++ index.js
@@ -37,18 +37,18 @@ player.on('trackStart', (queue, track) => {
     if (interaction.commandName === 'ping') {
       const row = new MessageActionRow()
         .addComponents(
-          new MessageButton()
-            .setCustomId('primary')
-            .setLabel('Primary')
-            .setStyle('PRIMARY'),
+          // ...
         );
   
-      await interaction.reply({ content: 'Pong!', components: [row] });
+      const embed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org')
+        .setDescription('Some description here');
+  
+      await interaction.reply({ content: 'Pong!', ephemeral: true, embeds: [embed], components: [row] });
     }
   });
-
-  console.log('track',track)
-console.log('queue',queue)
 });
 
 player.on('trackAdd', (queue, track) => {
